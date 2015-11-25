e93db963c332b1004a055d752afc1aa54e3f45c3
nicolas_vlachos <blaxomania@gmail.com>
2015-11-25 21:14:43 +0200

    required

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,12 +1,40 @@
+
 #include "SDL.h"
+#include <iostream>
 
-int main(int argc, char* args[])
+using namespace std;
+
+
+int main(int argc, char* argv[])
 {
-	//Start SDL
-	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window *window = nullptr;
+	SDL_Surface *windowSurface = nullptr;
+	SDL_Surface *imageSurface = nullptr;
 
-	//Quit SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+		cout << "Video Initialization Error: " << SDL_GetError() << endl;
+	else {
+		window = SDL_CreateWindow("SDL Coding Is F**HARD ", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 960, 540, SDL_WINDOW_SHOWN);
+		if (window == NULL)
+			cout << "Window Creation Error : " << SDL_GetError() << endl;
+		else {
+			windowSurface = SDL_GetWindowSurface(window);
+			imageSurface = SDL_LoadBMP("patrol.bmp");
+			if (imageSurface == NULL)
+				cout << "Image loading Error: " << SDL_GetError() << endl;
+			else {
+				SDL_BlitSurface(imageSurface, NULL, windowSurface, NULL);
+				SDL_UpdateWindowSurface(window);
+				SDL_Delay(4500);
+			}
+		}
+	}
+
+	SDL_DestroyWindow(window);
 	SDL_Quit();
 
+	//cout << "dfgd";
 	return 0;
+
+
 }
